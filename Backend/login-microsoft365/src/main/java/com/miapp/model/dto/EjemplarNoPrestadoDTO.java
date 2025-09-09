6b8172047f89fc20b2394499b0d94c5a78fe4723
Pedro-Moran <79230714+Pedro-Moran@users.noreply.github.com>
2025-09-09 10:49:20 -0500

    feat: mostrar detalles del ejemplar no prestado

--- Backend/login-microsoft365/src/main/java/com/miapp/model/dto/EjemplarNoPrestadoDTO.java
+++ Backend/login-microsoft365/src/main/java/com/miapp/model/dto/EjemplarNoPrestadoDTO.java
@@ -6,13 +6,26 @@ package com.miapp.model.dto;
 public class EjemplarNoPrestadoDTO {
     private Long idDetalle;
     private String titulo;
+    private String codigoLocalizacion;
+    private Long numeroIngreso;
+    private String autor;
+    private Integer anio;
 
     public EjemplarNoPrestadoDTO() {
     }
 
-    public EjemplarNoPrestadoDTO(Long idDetalle, String titulo) {
+    public EjemplarNoPrestadoDTO(Long idDetalle,
+                                 String titulo,
+                                 String codigoLocalizacion,
+                                 Long numeroIngreso,
+                                 String autor,
+                                 Integer anio) {
         this.idDetalle = idDetalle;
         this.titulo = titulo;
+        this.codigoLocalizacion = codigoLocalizacion;
+        this.numeroIngreso = numeroIngreso;
+        this.autor = autor;
+        this.anio = anio;
     }
 
     public Long getIdDetalle() {
@@ -29,5 +42,37 @@ public class EjemplarNoPrestadoDTO {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getCodigoLocalizacion() {
+        return codigoLocalizacion;
+    }
+
+    public void setCodigoLocalizacion(String codigoLocalizacion) {
+        this.codigoLocalizacion = codigoLocalizacion;
+    }
+
+    public Long getNumeroIngreso() {
+        return numeroIngreso;
+    }
+
+    public void setNumeroIngreso(Long numeroIngreso) {
+        this.numeroIngreso = numeroIngreso;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
     }
 }
