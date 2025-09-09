f0947b963c1967e7a37ae95457261417d9f447e4
Pedro-Moran <79230714+Pedro-Moran@users.noreply.github.com>
2025-09-09 02:52:18 -0500

    feat: integra almacenamiento de imágenes en bucket

--- Frontend/sakai-ng-master/src/environments/environment.ts
+++ Frontend/sakai-ng-master/src/environments/environment.ts
@@ -5,14 +5,15 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080/auth',
-    filesUrl:   'http://localhost:8080'
-  };
+    filesUrl: 'http://localhost:8080',
+    bucketUrl: 'https://backdev-campus.upsjb.edu.pe/api-oci-biblioteca-app'
+};
 
-  /*
-   * For easier debugging in development mode, you can import the following file
-   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
-   *
-   * This import should be commented out in production mode because it will have a negative impact
-   * on performance if an error is thrown.
-   */
-  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
